3c2e7e982157cef54f775327c7376cb0cb777938
Wout Kooijman <wapkooijman@gmail.com>
2017-06-12 14:04:29 +0200

    wrote code for baselines

--- LSTM/keras_lstm.py
+++ LSTM/keras_lstm.py
@@ -12,11 +12,18 @@ from sklearn.metrics import confusion_matrix
 from sklearn.utils import class_weight
 
 import numpy as np
+import random
 from functools import partial
 from itertools import product
 
 import lstm_functions
 import gather_data
+
+# allow_soft_placement=True
+# gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.3)
+
+# Filter compile warnings, errors still show
+# export TF_CPP_MIN_LOG_LEVEL=2
 
 ###############################################################################
 ###############################  Parmaters  ###################################
@@ -26,12 +33,14 @@ use_existing_data = True
 single_data_array = True
 shuffle = True
 normalize = True # misschien andere manier
+kfold = False
+nfolds = 10
 
 train_test_split = 0.9
 num_classes = 3
 seq_length = 1
-data_dim = 22
-random_seed = 985790549 #random.randint(0, 2**32-1)
+data_dim = 12
+random_seed = random.randint(0, 2**32-1) #985790549
 
 lstm_size = 128 # 256 of 512 misschien beter
 lstm_hidden_size = int((lstm_size+num_classes)/2)
@@ -44,9 +53,6 @@ val_split = 0.1
 ###############################################################################
 ###########################  Building the model  ##############################
 
-X, y = gather_data.get_data(use_existing_data, single_data_array, label_threshold)
-
-
 def w_categorical_crossentropy(y_true, y_pred, weights):
     nb_cl = len(weights)
     final_mask = K.zeros_like(y_pred[:, 0])
@@ -58,16 +64,7 @@ def w_categorical_crossentropy(y_true, y_pred, weights):
     return K.categorical_crossentropy(y_pred, y_true) * final_mask
 
 
-X = X[:, :data_dim]
-y = y[:]
-train_X, train_y, test_X, test_y = lstm_functions.process_data(X, 
-                                                            y, 
-                                                            normalize,
-                                                            shuffle,
-                                                            seq_length,
-                                                            random_seed,
-                                                            train_test_split,
-                                                            num_classes)
+
 
 w_array = np.array(((1, 1, 1), (1, 1, 1), (1, 1, 1)))
 w_array = {0: 1.1, 1: 1, 2:1.1}
@@ -75,12 +72,28 @@ ncce = partial(w_categorical_crossentropy, weights=w_array)
 ncce.__name__ ='w_categorical_crossentropy'
 
 
-model = Sequential()
-model.add(LSTM(input_shape = (seq_length, data_dim), units = lstm_size)) # TODO: recurrent dropout
-model.add(Dropout(drop_rate))
-model.add(Dense(lstm_hidden_size, activation='relu'))
-model.add(Dropout(drop_rate)) #TODO: test remove
-model.add(Dense(units = num_classes, activation='softmax'))
+def make_model(seq_length, data_dim, lstm_size, drop_rate, lstm_hidden_size, num_classes):
+    model = Sequential()
+    model.add(LSTM(input_shape = (seq_length, data_dim), units = lstm_size)) # TODO: recurrent dropout
+    model.add(Dropout(drop_rate))
+    model.add(Dense(lstm_hidden_size, activation='relu'))
+    model.add(Dropout(drop_rate)) #TODO: test remove
+    model.add(Dense(units = num_classes, activation='softmax'))
+    return model
+
+
+def fit_model(model, train_X, train_y, batch_size, no_epochs, val_split, callbacks, verbose=True):
+    return model.fit(
+        train_X,
+        train_y,
+        batch_size = batch_size,
+        epochs = no_epochs,
+        validation_split = val_split,
+        verbose=verbose,
+        shuffle=True,
+        callbacks = callbacks,
+        # class_weight = w_array
+    )
 
 
 ###############################################################################
@@ -89,27 +102,77 @@ model.add(Dense(units = num_classes, activation='softmax'))
 
 # optimizer = optimizers.Adam(lr=learning_rate)
 optimizer = optimizers.RMSprop(lr=learning_rate)
-model.compile(loss = 'categorical_crossentropy', optimizer = optimizer, metrics=['accuracy'])
-
 callbacks = [callbacks.EarlyStopping(monitor='val_loss', min_delta=0, patience=10, mode='auto')]
 
-model_fit = model.fit(
-    train_X,
-    train_y,
-    batch_size = batch_size,
-    epochs = no_epochs,
-    validation_split = val_split,
-    verbose=True,
-    shuffle=True,
-    callbacks = callbacks,
-    # class_weight = w_array
-)
+X, y = gather_data.get_data(use_existing_data, single_data_array, label_threshold)
+X = X[:, :data_dim]
+y = y[:]
 
-lstm_functions.get_statistics(model, 
+
+if kfold:
+    train_X_kfold, train_y_kfold, test_X_kfold, test_y_kfold = lstm_functions.process_data(X,
+        y, 
+        normalize,
+        shuffle,
+        seq_length,
+        random_seed,
+        train_test_split,
+        num_classes, 
+        kfold,
+        nfolds)
+
+    total_accuracy = 0
+
+    for i in range(0, nfolds):
+        train_X = train_X_kfold[i]
+        train_y = train_y_kfold[i]
+        test_X = test_X_kfold[i]
+        test_y = test_y_kfold[i]
+        model = None # Clear model
+        model = make_model(seq_length, data_dim, lstm_size, drop_rate, lstm_hidden_size, num_classes)
+        model.compile(loss = 'categorical_crossentropy', optimizer = optimizer, metrics=['accuracy'])
+        print("Training %d fold out of %d..." % ((i+1), nfolds))
+        model_fit = fit_model(model, train_X, train_y, batch_size, no_epochs, val_split, callbacks, verbose=0)
+        acc = lstm_functions.get_statistics(model, 
                             model_fit, 
                             X, 
                             train_X, 
                             test_X, 
                             train_y, 
                             test_y, 
-                            seq_length)
+                            seq_length,
+                            kfold,
+                            nfolds,
+                            showPlots=False)
+
+        total_accuracy = total_accuracy + acc
+
+    total_accuracy = total_accuracy/nfolds
+
+    print("Average accuracy = %f" % total_accuracy)
+
+else:
+    train_X, train_y, test_X, test_y = lstm_functions.process_data(X, 
+                                                            y, 
+                                                            normalize,
+                                                            shuffle,
+                                                            seq_length,
+                                                            random_seed,
+                                                            train_test_split,
+                                                            num_classes, 
+                                                            kfold,
+                                                            nfolds)
+    model = make_model(seq_length, data_dim, lstm_size, drop_rate, lstm_hidden_size, num_classes)
+    model.compile(loss = 'categorical_crossentropy', optimizer = optimizer, metrics=['accuracy'])
+    model_fit = fit_model(model, train_X, train_y, batch_size, no_epochs, val_split, callbacks, verbose=2)
+    lstm_functions.get_statistics(model, 
+                            model_fit, 
+                            X, 
+                            train_X, 
+                            test_X, 
+                            train_y, 
+                            test_y, 
+                            seq_length,
+                            kfold,
+                            nfolds,
+                            showPlots=True)
--- LSTM/lstm_functions.py
+++ LSTM/lstm_functions.py
@@ -35,7 +35,7 @@ def normalize_data(data):
     return(data - mean) / std
 
 
-def process_data(X, y, normalize, shuffle, seq_length, random_seed, train_test_split, num_classes):
+def process_data(X, y, normalize, shuffle, seq_length, random_seed, train_test_split, num_classes, kfold, nfold):
     sequence_length = seq_length + 1
 
     if normalize:
@@ -51,22 +51,50 @@ def process_data(X, y, normalize, shuffle, seq_length, random_seed, train_test_s
     result = np.array(result)
 
     if shuffle:
-        print(random_seed)
+        print("Random seed = %d" % random_seed)
         np.random.seed(random_seed)
         np.random.shuffle(result)
 
-    split_index = int(train_test_split * result.shape[0])
-    x_train = result[:split_index, :seq_length, :-1]
-    x_test = result[split_index:, :seq_length, :-1]
+    if kfold:
+        index_part = int(result.shape[0]/nfold)
+        x_train_kfold = []
+        y_train_kfold = []
+        x_test_kfold = []
+        y_test_kfold = []
+
+        for i in range(0, nfold):
+            x_test_kfold.append(result[(i*index_part):(index_part*(i+1)), :seq_length, :-1])
+            y_test_kfold.append(make_one_hot(result[(i*index_part):(index_part*(i+1)), 0, -1], num_classes))
+
+            x_train1 = result[:(i*index_part), :seq_length, :-1]
+            x_train2 = result[index_part*(i+1): , :seq_length, :-1]
+            x_train_kfold.append(np.vstack((x_train1, x_train2)))
+
+            y_train1 = result[:(i*index_part), 0, -1]
+            y_train1 = y_train1.reshape((y_train1.shape[0], 1))
+
+            y_train2 = result[index_part*(i+1): , 0, -1]
+            y_train2 = y_train2.reshape((y_train2.shape[0], 1))
+            y_train_kfold.append(make_one_hot(np.vstack((y_train1, y_train2)), num_classes))
+        return [np.array(x_train_kfold),
+                np.array(y_train_kfold),
+                np.array(x_test_kfold),
+                np.array(y_test_kfold)]
+            
+
+    else:
+        split_index = int(train_test_split * result.shape[0])
+        x_train = result[:split_index, :seq_length, :-1]
+        x_test = result[split_index:, :seq_length, :-1]
 
 
-    y_train = result[:split_index, 0, -1]
-    y_test = result[split_index:, 0, -1]
+        y_train = make_one_hot(result[:split_index, 0, -1], num_classes)
+        y_test = make_one_hot(result[split_index:, 0, -1], num_classes)
 
-    x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], x_train.shape[2]))
-    x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], x_test.shape[2]))
+        x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], x_train.shape[2]))
+        x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], x_test.shape[2]))
 
-    return [x_train, make_one_hot(y_train, num_classes), x_test, make_one_hot(y_test, num_classes)]
+        return [x_train, y_train, x_test, y_test]
 
 
 def predict(model, data, window_size):
@@ -162,36 +190,43 @@ def get_data_plots(X):
     nopos = X[:, 14]
     noneg = X[:, 15]
     x_axis = 'days after 1 jan 2014'
-    y_axis = 'btc price'
+    y_axis = 'btc price (in US Dollar)'
     make_plot_2(price, google, x_axis, y_axis, 'Google query amount', 'google query amount vs bitcoin price')
     make_plot_2(price, noposts, x_axis, y_axis, 'Number of bitcoin related posts on reddit', 'No. of bitcoin posts on reddit vs bitcoin price')
     make_plot_2(price, nopos, x_axis, y_axis, 'Number of positive posts on reddit', 'No. of positive bitcoin posts on reddit vs bitcoin price')
     make_plot_2(price, noneg, x_axis, y_axis, 'Number of negative posts on reddit', 'No. of negative bitcoin posts on reddit vs bitcoin price')
 
 
-def get_statistics(model, model_fit, X, train_X, test_X, train_y, test_y, seq_length):
+def get_statistics(model, model_fit, X, train_X, test_X, train_y, test_y, seq_length, kfold, nfolds, showPlots):
+    
     predictions_train, probabilities_train = predict(model, train_X, seq_length)
     
     print("Percentage correct trainset: ", error(np.array(predictions_train), train_y) * 100, "%")
 
     predictions, probabilities = predict(model, test_X, seq_length)
+    predictions = np.roll(np.argmax(test_y, axis=1), 1, axis=0)
+
+    accuracy = error(np.array(predictions), test_y) * 100
 
     print("Real: ", np.argmax(test_y, axis=1))
     print("Predicted: ", predictions)
-    print("Percentage correct testset: ", error(np.array(predictions), test_y) * 100, "%")
+    print("Percentage correct testset: ", accuracy, "%")
 
-    make_prediction_plot(np.argmax(test_y, axis=1), predictions)
-    loss = model_fit.history.get('val_loss')
-    make_plot(loss, 'Training epochs', 'Loss', 'Training loss on validation set')
-    acc = model_fit.history.get('val_acc')
-    make_plot(acc, 'Accuracy', 'Training epochs', 'Training accuracy on validation set')
-    ent = calculate_entropy(probabilities_train)
-    make_plot(ent, 'Training epochs', 'Entropy', 'Entropy over training period')
+    if showPlots:
+        make_prediction_plot(np.argmax(test_y, axis=1), predictions)
+        loss = model_fit.history.get('val_loss')
+        make_plot(loss, 'Training epochs (in days)', 'Loss', 'Training loss on validation set')
+        acc = model_fit.history.get('val_acc')
+        make_plot(acc, 'Accuracy (in percentage)', 'Training epochs (in days)', 'Training accuracy on validation set')
+        ent = calculate_entropy(probabilities_train)
+        make_plot(ent, 'Training epoch (in days)', 'Entropy', 'Entropy over training period')
 
-    cnf_matrix = confusion_matrix(np.argmax(test_y, axis=1), predictions)
-    class_names = ['Fall', 'Stay', 'Rise']
-    plt.figure()
-    name = 'Confusion matrix of test data consisting of %d days' %len(predictions)
-    plot_confusion_matrix(cnf_matrix, class_names, title=name)
-    plt.show()
-    get_data_plots(X)
+        cnf_matrix = confusion_matrix(np.argmax(test_y, axis=1), predictions)
+        class_names = ['Fall', 'Stay', 'Rise']
+        plt.figure()
+        name = 'Confusion matrix of test data consisting of %d days' %len(predictions)
+        plot_confusion_matrix(cnf_matrix, class_names, title=name)
+        plt.show()
+        get_data_plots(X)
+    if kfold:
+        return accuracy
